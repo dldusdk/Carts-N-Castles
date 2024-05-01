d3eed492e09c5b3894007a33c0648a218b22bb36
Michelle <michelleyeonalee@gmail.com>
2024-05-02 11:45:04 +1200

    - Created difficulty (hard, normal) methods; - Created play method     - play makes difficulty buttons visible - Added javadoc files

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"seng201.team0"},{"l":"seng201.team0.gui"},{"l":"seng201.team0.models"},{"l":"seng201.team0.services"}];updateSearchResults();
--- src/main/java/seng201/team0/gui/MainController.java
+++ src/main/java/seng201/team0/gui/MainController.java
@@ -6,28 +6,68 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * Controller for the MainScreen
- * @author Michelle Lee
- */
-
 public class MainController {
+    /**
+     * Main Controller for the initial start up screen of game.
+     * @author Michelle Lee
+     */
+
 
     @FXML
     private Button quitButton;
     @FXML
     private Button playButton;
     @FXML
+    private Button normalButton;
+    @FXML
+    private Button hardButton;
+
+    @FXML
     private AnchorPane mainscreenPane;
+    @FXML
+    //private Label creditsLabel;
 
     Stage stage;
 
+    public void play(ActionEvent actionEvent) {
+        /**
+         * Method for when the Play button is clicked, to show difficulty buttons
+         * @author Michelle Lee
+         */
+        playButton.setVisible(false); //hide button
+        quitButton.setVisible(false); // hide quit button
+        //creditsLabel.setVisible(false); // hide label
+        normalButton.setVisible(true);
+        hardButton.setVisible(true);
+
+    }
     @FXML
-    public void quit(ActionEvent actionEvent)  {
+    public void normal(ActionEvent actionEvent) {
+        /**
+         * When Normal Button is clicked, run askName popup box and initialize game
+         * @author Michelle Lee
+         */
+        hardButton.setVisible(false); // code for normal difficulty goes here visibility just place holder code
+    }
+    @FXML
+    public void hard(ActionEvent actionEvent) {
+        /**
+         * When Hard Button is clicked, run askName popup box and initialize game
+         * @author Michelle Lee
+         */
+        normalButton.setVisible(false); // code for hard difficulty goes here
+
+    }
+
+    @FXML
+    public void quit(ActionEvent actionEvent) {
+        /**
+         * Method that confirms whether a user wants to quit by opening a dialog box
+         * @author Michelle Lee
+         */
 
         // Have a pop-up appear if the user clicks 'Quit' Button
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -35,13 +75,22 @@ public class MainController {
         alert.setHeaderText("You are about to quit our game ˙◠˙ ");
         alert.setContentText("Are you sure you want to quit?");
 
-        // if they want to quit, close program
+        // If User clicks 'Confirm', Quit game
         if (alert.showAndWait().get() == ButtonType.OK) {
             stage = (Stage) mainscreenPane.getScene().getWindow();
             System.out.println("You Successfully quit the game!");
             stage.close();
         }
     }
+
+    public void init(Stage primaryStage) {
+    }
+
+}
+
+
+
+    /**
 
     @FXML
     public void play(ActionEvent actionEvent){
@@ -53,7 +102,8 @@ public class MainController {
             e.printStackTrace();
     }}
 
+
     public void init(Stage primaryStage) {
     }
+    **/
 
-}

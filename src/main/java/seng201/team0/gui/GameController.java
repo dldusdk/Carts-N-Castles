81cff753a66c1b7754c982dcbb8b61925904355e
Michelle <michelleyeonalee@gmail.com>
2024-05-07 23:37:32 +1200

    Updated: - gameScreen.fxml to have correct dimensions - all map sizes must be 1152pxw x 1024

--- src/main/java/seng201/team0/gui/GameController.java
+++ src/main/java/seng201/team0/gui/GameController.java
@@ -28,11 +28,8 @@ public class GameController {
     /**
      * Initializes the controller with the primary stage
      * @param primaryStage The primary stage of the application
-     *
-     *  NOTE FOR GORDON: Sorry I couldn't get your code working with the new fxml, build errors blah
-     *  Your code is in Gordons.java, you just need to unmatch the comment paranteheses I had to comment it out
-     *  to make sure this fxml was gonna run.
      */
+
     public void init(Stage primaryStage) {
         this.primaryStage = primaryStage;
         // Add any other initialization logic here

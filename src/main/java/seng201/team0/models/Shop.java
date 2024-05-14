9764ee667926f9b0a05ec392b4d31cba832c3b4a
Michelle <michelleyeonalee@gmail.com>
2024-05-14 13:41:41 +1200

    Implemented: - All three towers able to be placed on map after clicking the button - Resets the mouseevent to be 'out of shop' after clicked
    To Do:
    - Implement logic for checking if valid
    - Rest of shop
    - Seperate tower methods? check with Gordon on wed
    - Tower STATS and STAT GUI
    - Inventory GUI

--- src/main/java/seng201/team0/models/Shop.java
+++ src/main/java/seng201/team0/models/Shop.java
@@ -1,25 +1,30 @@
 package seng201.team0.models;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class Shop {
+
     // Player info
     int currentStock;
     @FXML
     Label playerCoins;
+
     // Items
     String itemName;
     String itemDescription;
     int itemCost;
+
     // Towers
     String towerName;
     String towerDescription;
     int towerCost;
+    int silvertowerStock;
+    int bronzetowerStock;
+    int goldtowerStock;
+
     // Upgrades
     String upgradeName;
     String upgradeDescription;
@@ -33,8 +38,20 @@ public class Shop {
 
     }
 
-    public void towers() {
+    public int towerStock() {
+        /**
+         * Set tower information here:
+         * RE: TOWER STOCK. Tower stock resets per round??? Check with gordon
+         * RANDOMISE TOWER STOCK???
+         * MAX STOCK is 5
+         * 5 on map
+         * Error if more than 5" YOU CAN ONLY have 5 towers at a time, please purchase after you sell a tower.
+         */
+        silvertowerStock = 2;
+        bronzetowerStock = 2;
+        goldtowerStock = 1;
 
+        return silvertowerStock + bronzetowerStock + goldtowerStock;
     }
 
     public void upgrades() {
